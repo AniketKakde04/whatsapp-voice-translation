2df57333501a05f3b6d88181b03372822bb9cf8a
Aniket kakde <aniket.kakde@Indussoft.com>
2025-07-15 11:04:23 +0530

    changes in gemini_utils.py

--- security_utils.py
+++ security_utils.py
@@ -1,12 +1,12 @@
-import os
-from cryptography.fernet import Fernet
-from dotenv import load_dotenv
+# import os
+# from cryptography.fernet import Fernet
+# from dotenv import load_dotenv
 
-load_dotenv()
-fernet = Fernet(os.getenv("FERNET_KEY").encode())
+# load_dotenv()
+# fernet = Fernet(os.getenv("FERNET_KEY").encode())
 
-def encrypt_text(text: str) -> str:
-    return fernet.encrypt(text.encode()).decode()
+# def encrypt_text(text: str) -> str:
+#     return fernet.encrypt(text.encode()).decode()
 
-def decrypt_text(token: str) -> str:
-    return fernet.decrypt(token.encode()).decode()
+# def decrypt_text(token: str) -> str:
+#     return fernet.decrypt(token.encode()).decode()
